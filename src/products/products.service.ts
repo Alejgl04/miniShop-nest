3646bebbbb81231700d172c832b53970acd153ba
Alejandro Guerrero <alejo.jesus.magne@gmail.com>
2022-09-03 20:11:49 -0400

    pagination and search

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,9 +1,13 @@
 import { Injectable, InternalServerErrorException, Logger, BadRequestException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from '../common/dtos/pagination.dto';
+
 import { Product } from './entities/product.entity';
+import { validate as isUUID } from 'uuid'; 
 
 @Injectable()
 export class ProductsService {
@@ -30,17 +34,35 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return this.productRespository.find();
+  findAll( pagination: PaginationDto) {
+
+    const { limit = 10, offset = 0} = pagination;
+
+    return this.productRespository.find({
+      take: limit,
+      skip: offset,
+      //TODO: relations
+    });
   }
 
-  async findOne(id: string) {
+  async findOne( term: string) {
     
-    const product = await this.productRespository.findOneBy({ id });
+    let product: Product;
+
+    if ( isUUID(term) ) {
+      product = await this.productRespository.findOneBy({ id: term });
+    }
+    else{
+      const queryBuilder = this.productRespository.createQueryBuilder();
+      product = await queryBuilder
+        .where('UPPER(title) =:title or slug =:slug', {
+          title: term.toUpperCase(),
+          slug: term.toLowerCase()
+        }).getOne();
+    }
 
     if ( !product ) {
-      throw new NotFoundException(`Product with id ${ id } not found`);
-      
+      throw new NotFoundException(`Product with id ${ term } not found`);
     }
 
     return product;
