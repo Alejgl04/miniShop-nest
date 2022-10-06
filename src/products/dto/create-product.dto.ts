73915d726da0ed497b8a754dd1fa9719f9784557
Alejandro Guerrero <alejo.jesus.magne@gmail.com>
2022-10-05 21:08:55 -0400

    dto property api swagger

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,41 +1,55 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString, MinLength, IsNumber, IsPositive, IsOptional, IsInt, IsArray, IsIn } from "class-validator";
 
 export class CreateProductDto {
 
+  @ApiProperty({
+    description: 'Product title (unique)',
+    nullable: false,
+    minLength: 3
+  })
   @IsString()
   @MinLength(2)
   title: string;
 
+  @ApiProperty()
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty()
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty()
   @IsIn(['men','woman','kid','unisex'])
   gender: string;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags: string[];
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
